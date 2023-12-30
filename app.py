93546bb5cd825f160caad6d00c61b9135d163d6a
Revati Yadav <83214608+R2000176@users.noreply.github.com>
2023-12-31 01:02:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import bcrypt
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-#uri = "mongodb+srv://redblack:Password123@cluster0.t6qvw7d.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://redblack:Password123@cluster0.t6qvw7d.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
-#client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 
 app = Flask(__name__, static_url_path='/static')
